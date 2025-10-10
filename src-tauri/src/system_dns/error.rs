975bad39df0d09a9774ce797aaf30374a89d110b
Ali Ghahremani <alighahremani1377@gmail.com>
2025-10-10 16:24:40 +0330

    feat: implement io error to SystemDNSError type conversion + Permission Denied

--- src-tauri/src/system_dns/error.rs
+++ src-tauri/src/system_dns/error.rs
@@ -1,14 +1,27 @@
 use std::fmt::Display;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Debug)]
 pub enum SystemDNSError {
     NotSupported,
+    PermissionDenied,
+    IOError(std::io::Error),
 }
 
 impl Display for SystemDNSError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SystemDNSError::NotSupported => f.write_str("NotSupported"),
+            SystemDNSError::NotSupported => f.write_str("Not Supported"),
+            SystemDNSError::PermissionDenied => f.write_str("PermissionDenied"),
+            SystemDNSError::IOError(e) => f.write_fmt(format_args!("IOError: {}", e)),
+        }
+    }
+}
+
+impl From<std::io::Error> for SystemDNSError {
+    fn from(value: std::io::Error) -> Self {
+        match value.kind() {
+            std::io::ErrorKind::PermissionDenied => Self::PermissionDenied,
+            _ => Self::IOError(value),
         }
     }
 }
